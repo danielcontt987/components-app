40f1080fb37891b379735e7aa0646cc1c2174ac9
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2026-03-02 16:54:49 -0600

    Animaciones 101

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,58 @@
-import { View, Text } from 'react-native';
+import ThemeButton from '@/presentation/shared/ThemeButton';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useRef } from 'react';
+import { Animated, Easing } from 'react-native';
 
 const Animation101Screen = () => {
+
+  const animatedOpcity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-100)).current;
+
+  const fadeIn = () => {
+    Animated.timing(animatedOpcity,{
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+
+    Animated.timing(animatedTop, {
+       toValue: 0,
+       duration: 700,
+       useNativeDriver: true,
+       easing: Easing.bounce
+    }).start();
+  }
+
+  const fadeOut = () => {
+    Animated.timing(animatedOpcity,{
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true
+    }).start(() => animatedTop.resetAnimation());
+  }
+
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemedView margin className='justify-center items-center'>
+      <Animated.View 
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl"
+        style={{
+          width: 150,
+          height: 150,
+          opacity: animatedOpcity,
+          transform:[
+            {
+              translateY: animatedTop
+            }
+          ]
+        }}
+      />
+      <ThemeButton className='my-5' onPress={fadeIn}>
+        FadeIn
+      </ThemeButton>
+      <ThemeButton className='my-5' onPress={fadeOut}>
+          FadeOn
+      </ThemeButton>
+    </ThemedView>
   );
 };
 export default Animation101Screen;
---
+++ presentation/shared/ThemeButton.tsx
@@ -0,0 +1,21 @@
+import { Pressable, PressableProps, Text } from 'react-native';
+
+interface Props extends PressableProps{
+    onPress: () => void;
+    className?: string,
+    children: string,
+
+}
+
+const ThemeButton = ({onPress, className, children}: Props) => {
+  return (
+    <Pressable
+        onPress={onPress}
+        className={`bg-light-primary dark:bg-dark-primary items-center rounded-xl px-6 py-4 active:opacity-80 ${className}`}
+    >
+      <Text className='text-white text-xl'>{children}</Text>
+    </Pressable>
+  )
+}
+
+export default ThemeButton
