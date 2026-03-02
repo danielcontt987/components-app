b39a5e80f6567f7466f0dc21f619344f5cc18515
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2026-03-01 18:55:59 -0600

    creando mi propio tema oscuro y blanco ademas de uso de children y del rest

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,24 +5,27 @@ import 'react-native-reanimated';
 import "./global.css";
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import { Text, View } from 'react-native';
+import { useThemeColor } from '@/hooks/use-theme-color';
+import ThemedView from '@/presentation/shared/ThemedView';
+import ThemeText from '@/presentation/shared/ThemeText';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor({}, 'background');
   const colorScheme = useColorScheme();
 
+
   return (
-    <GestureHandlerRootView>
+    <GestureHandlerRootView style={{backgroundColor: backgroundColor, flex: 1}}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         {/* <Stack>
         </Stack> */}
-        <View className="bg-light-backgorund dark:bg-dark-backgorund">
-          <Text className='mt-10 text-3xl text-light-primary dark:text-dark-primary'>Hola mundo</Text>
-
-        </View>
+        <ThemedView margin safe>
+          <ThemeText type='h1'>Hola mundo</ThemeText>
+        </ThemedView>
         <StatusBar style="auto" />
       </ThemeProvider>
     </GestureHandlerRootView>
---
+++ presentation/shared/ThemeText.tsx
@@ -0,0 +1,29 @@
+import { Text, TextProps } from 'react-native';
+
+type textType = 'normal' | 'h1' | 'h2' | 'semi-bold' | 'link';
+interface Props extends TextProps{
+    className?: string;
+    type?: textType;
+
+
+}
+
+const ThemeText = ({
+    className,
+    type,
+    ...rest
+}: Props) => {
+  return (
+      <Text className={[
+        'text-light-text dark:text-dark-text',
+        type=== 'normal' ? 'font-normal' : undefined,
+        type=== 'h1' ? 'text-3xl' : undefined,
+        type=== 'h2' ? 'text-xl' : undefined,
+        type=== 'semi-bold' ? 'font-semibold' : undefined,
+        type=== 'link' ? 'font-normal underline' : undefined,
+        className
+      ].join(' ')} {...rest} />
+  )
+}
+
+export default ThemeText
