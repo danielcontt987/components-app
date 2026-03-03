543f037e61d4fc755147c9540b03a615021edae4
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2026-03-02 18:28:54 -0600

    Animaciones 102

--- app/animation-102/index.tsx
+++ app/animation-102/index.tsx
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native';
+import ThemedView from "@/presentation/shared/ThemedView";
+import { useRef } from "react";
+import { Animated, PanResponder, StyleSheet } from "react-native";
 
 const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: pan.x,
+          dy: pan.y,
+        },
+      ],
+      { useNativeDriver: false }
+    ),
+    onPanResponderRelease: () => {
+      Animated.spring(pan, {
+        toValue: { x: 0, y: 0 },
+        useNativeDriver: false,
+      }).start();
+    },
+  });
+
   return (
-    <View>
-      <Text>Animation102Screen</Text>
-    </View>
+    <ThemedView margin className="justify-center items-center">
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.box]}
+      />
+    </ThemedView>
   );
 };
+
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+  }, 
+  box: { 
+    backgroundColor: '#61dafb', 
+    width: 80, 
+    height: 80, 
+    borderRadius: 4, }, }); 
 export default Animation102Screen;
