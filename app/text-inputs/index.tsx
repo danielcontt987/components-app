6912b57ae4dd07ee1a2564f3fd65197a7fce9f35
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2026-03-04 16:11:15 -0600

    KeyboardAvoidingView para medir el hight de la pantalla

--- app/text-inputs/index.tsx
+++ app/text-inputs/index.tsx
@@ -3,35 +3,101 @@ import ThemedInput from '@/presentation/shared/ThemedInput';
 import ThemedView from '@/presentation/shared/ThemedView';
 import ThemeText from '@/presentation/shared/ThemeText';
 import { useState } from 'react';
+import { KeyboardAvoidingView, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const TextInputsScreen = () => {
   const [form, setForm] = useState({
     name: '',
-    emial: '',
+    email: '',
     phone: ''
   });
 
   return (
-    <ThemedView margin>
-      <ThemeCard className='mb-4'>
-        {/* <TextInput
+    <KeyboardAvoidingView behavior='height'>
+      <ScrollView>
+        <ThemedView margin>
+          <ThemeCard className='mb-4'>
+            {/* <TextInput
           placeholder='nombre completo'
           autoCapitalize={'words'}
           autoCorrect={false}
           onChangeText={(text) => setForm({ ...form, name: text })}
         /> */}
-        <ThemedInput 
-          placeHolder='Nombre' 
-          onChangeText={(text) => setForm({...form, name: text})} 
-          keyboardType='numeric'
-          />
-      </ThemeCard>
-      <ThemeCard>
-        <ThemeText>
-          {JSON.stringify(form, null, 2)}
-        </ThemeText>
-      </ThemeCard>
-    </ThemedView>
+            <ThemedInput
+              placeHolder='Nombre'
+              onChangeText={(text) => setForm({ ...form, name: text })}
+              keyboardType='default'
+            />
+
+            <ThemedInput
+              placeHolder='Correo electronico'
+              onChangeText={(text) => setForm({ ...form, email: text })}
+              keyboardType='email-address'
+            />
+
+            <ThemedInput
+              placeHolder='Telefono'
+              onChangeText={(text) => setForm({ ...form, phone: text })}
+              keyboardType='numeric'
+            />
+          </ThemeCard>
+          <ThemeCard className="my-2">
+            <ThemeText>
+              {JSON.stringify(form, null, 2)}
+            </ThemeText>
+          </ThemeCard>
+          <ThemeCard className="my-2">
+            <ThemeText>
+              {JSON.stringify(form, null, 2)}
+            </ThemeText>
+          </ThemeCard>
+          <ThemeCard className="my-2">
+            <ThemeText>
+              {JSON.stringify(form, null, 2)}
+            </ThemeText>
+          </ThemeCard>
+          <ThemeCard className="my-2">
+            <ThemeText>
+              {JSON.stringify(form, null, 2)}
+            </ThemeText>
+          </ThemeCard>
+          <ThemeCard className="my-2">
+            <ThemeText>
+              {JSON.stringify(form, null, 2)}
+            </ThemeText>
+          </ThemeCard>
+          <ThemeCard className="my-2">
+            <ThemeText>
+              {JSON.stringify(form, null, 2)}
+            </ThemeText>
+          </ThemeCard>
+          <ThemeCard className="my-2">
+            <ThemeText>
+              {JSON.stringify(form, null, 2)}
+            </ThemeText>
+          </ThemeCard>
+          <ThemeCard className="my-2">
+            <ThemeText>
+              {JSON.stringify(form, null, 2)}
+            </ThemeText>
+          </ThemeCard>
+          <ThemeCard className="my-2">
+            <ThemeText>
+              {JSON.stringify(form, null, 2)}
+            </ThemeText>
+          </ThemeCard>
+          <ThemeCard>
+            <ThemedInput
+              placeHolder='Telefono'
+              onChangeText={(text) => setForm({ ...form, phone: text })}
+              keyboardType='numeric'
+            />
+          </ThemeCard>
+        </ThemedView>
+        <View style={{marginBottom: 100}} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 export default TextInputsScreen;
