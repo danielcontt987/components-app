1b4940a06d35c7acc6f5f5714493f6c7423309ba
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2026-03-04 16:27:47 -0600

    Pull to refresh

--- app/pull-to-refresh/index.tsx
+++ app/pull-to-refresh/index.tsx
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native';
+import { useThemeColor } from '@/hooks/use-theme-color';
+import ThemedView from '@/presentation/shared/ThemedView';
+import ThemeText from '@/presentation/shared/ThemeText';
+import { useState } from 'react';
+import { RefreshControl } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const PullToRefreshScreen = () => {
+
+  const primaryColor = useThemeColor({}, 'primary')
+  const [isRefresing, setIsRefresing] = useState(false)
+  const onRefresh = async() => {
+    setIsRefresing(true);
+
+    setTimeout(() => {
+      setIsRefresing(false)
+    },3000)
+  }
+
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl 
+          refreshing={isRefresing}
+          onRefresh={onRefresh}
+          colors={[primaryColor, 'red', 'orange', 'green']}
+        />
+      }
+    >
+      <ThemedView margin>
+        <ThemeText>PullToRefreshScreen</ThemeText>
+      </ThemedView>
+    </ScrollView>
   );
 };
 export default PullToRefreshScreen;
